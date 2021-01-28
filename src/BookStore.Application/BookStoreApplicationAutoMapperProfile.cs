de21754f87709be13ee26a0a0922271569606ad6
Павло Горенко <gorenkopk@gmail.com>
2021-01-28 20:08:13 +0200

    Add files for Create, Update Book entity. Add data transfer objects mapper.

--- src/BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using BookStore.Books;
 
 namespace BookStore
 {
@@ -6,9 +7,8 @@ namespace BookStore
     {
         public BookStoreApplicationAutoMapperProfile()
         {
-            /* You can configure your AutoMapper mapping configuration here.
-             * Alternatively, you can split your mapping configurations
-             * into multiple profile classes for a better organization. */
+            CreateMap<Book, BookDto>();
+            CreateMap<CreateUpdateBookDto, Book>();
         }
     }
 }
