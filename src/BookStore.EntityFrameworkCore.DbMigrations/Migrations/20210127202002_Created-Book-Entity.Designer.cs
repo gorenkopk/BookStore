34680aa1a0af85d8df837eddcda12f605aa866c9
Павло Горенко <gorenkopk@gmail.com>
2021-01-28 05:43:45 +0200

    Add book data seeds before migration

--- src/BookStore.EntityFrameworkCore.DbMigrations/Migrations/20210127202002_Created-Book-Entity.Designer.cs
+++ src/BookStore.EntityFrameworkCore.DbMigrations/Migrations/20210127202002_Created-Book-Entity.Designer.cs
@@ -4,15 +4,17 @@ using BookStore.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace BookStore.Migrations
 {
     [DbContext(typeof(BookStoreMigrationsDbContext))]
-    partial class BookStoreMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210127202002_Created-Book-Entity")]
+    partial class CreatedBookEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
